80af90881325dfc795c439a40cdf168192370545
LeeGaHeee <0415gahui@gmail.com>
2022-11-03 01:32:27 +0900

    Create Project

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.createApp(App).mount('#app')
+
+import { createApp } from 'vue'
+import App from './App'
+
+createApp(App).mount('#app')
